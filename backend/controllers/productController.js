dcaa81d91bc073d227cef942e4b5bfef457637ea
Vishal <vishaldas6566@gmail.com>
2023-08-02 13:59:53 +0530

    create product route added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,19 @@
-exports.getAllProducts=(req,res)=>{
-    res.status(200).json({message:"route is working fine"})
+const Product=require("../models/productModel")
+
+//Create Product  --Admin
+ exports.createProduct=async(req,res,next)=>{
+    const product=await Product.create(req.body);
+    res.status(201).json({
+        success:true,
+        product
+    })
+    }
+
+//get all product
+exports.getAllProducts=async(req,res)=>{
+    const products=await Product.find()
+    res.status(200).json({
+        Success:true,
+        Products:products
+    })
 }
