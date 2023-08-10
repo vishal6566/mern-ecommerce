829cf38778caad6a8a40895c36730d0131b7fa65
Vishal <vishaldas6566@gmail.com>
2023-08-10 13:14:51 +0530

    search functionality added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorhandler");
-const catchAsyncHandler=require("../middlewares/catchAsyncError")
+const catchAsyncHandler=require("../middlewares/catchAsyncError");
+const ApiFeatures = require("../utils/apiFeatures");
 //Create Product  --Admin
 exports.createProduct =catchAsyncHandler(async (req, res, next) => {
     const product = await Product.create(req.body);
@@ -12,7 +13,8 @@ exports.createProduct =catchAsyncHandler(async (req, res, next) => {
 
 //get all product
 exports.getAllProducts =catchAsyncHandler(( async (req, res,next) => {
-    const products = await Product.find()
+    const apiFeature=new ApiFeatures(Product.find(),req.query).serach()
+    const products = await apiFeature.query
     res.status(200).json({
         Success: true,
         Products: products
