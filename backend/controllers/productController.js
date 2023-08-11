22846a28dbb046caedc9f8354e8dfb43e0ceb309
Vishal <vishaldas6566@gmail.com>
2023-08-11 11:23:50 +0530

    pagination feature added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,72 +1,74 @@
 const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorhandler");
-const catchAsyncHandler=require("../middlewares/catchAsyncError");
+const catchAsyncHandler = require("../middlewares/catchAsyncError");
 const ApiFeatures = require("../utils/apiFeatures");
 //Create Product  --Admin
-exports.createProduct =catchAsyncHandler(async (req, res, next) => {
-    const product = await Product.create(req.body);
-    res.status(201).json({
-        success: true,
-        product
-    })
-}) 
+exports.createProduct = catchAsyncHandler(async (req, res, next) => {
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    success: true,
+    product,
+  });
+});
 
 //get all product
-exports.getAllProducts =catchAsyncHandler(( async (req, res,next) => {
-    const apiFeature=new ApiFeatures(Product.find(),req.query).serach().filter()
-    const products = await apiFeature.query
-    res.status(200).json({
-        Success: true,
-        Products: products
-    })
-}
-))
+exports.getAllProducts = catchAsyncHandler(async (req, res) => {
+  const resultPerPage = 5;
+const productCount= await Product.countDocuments()
+  const apiFeature = new ApiFeatures(Product.find(), req.query)
+    .serach()
+    .filter()
+    .pagination(resultPerPage);
+  const products = await apiFeature.query;
+  res.status(200).json({
+    Success: true,
+    Products: products,
+    productCount
+  });
+});
 //update product  --admin
 
-exports.updateProduct =catchAsyncHandler(async (req, res, next) => {
-    let product = await Product.findById(req.params.id);
-    if (!product) {
-        return next(new ErrorHandler("Product not found", 404))
-    }
-    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true,
-        useFindAndModify: false
-    })
+exports.updateProduct = catchAsyncHandler(async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
 
-    res.status(200).json({
-        success: true,
-        product
-    })
-}) 
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
 
 //get product details
 
-exports.getProductDetails =catchAsyncHandler(async (req, res, next) => {
-    const product = await Product.findById(req.params.id)
+exports.getProductDetails = catchAsyncHandler(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
 
-    if (!product) {
-        return next(new ErrorHandler("Product not found", 404))
-    }
-    res.status(200).json({
-        success: true,
-        product
-    })
-}
-) 
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
 //Delete Product --Admin
 
-exports.deleteProduct =catchAsyncHandler(async (req, res, next) => {
-    const product = await Product.findByIdAndDelete(req.params.id)
+exports.deleteProduct = catchAsyncHandler(async (req, res, next) => {
+  const product = await Product.findByIdAndDelete(req.params.id);
 
-    if (!product) {
-        return next(new ErrorHandler("Product not found", 404))
-    }
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
 
-
-
-    await res.status(200).json({
-        success: true,
-        message: "Product deleted successfully."
-    })
-}) 
+  await res.status(200).json({
+    success: true,
+    message: "Product deleted successfully.",
+  });
+});
--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -1,32 +1,48 @@
 class ApiFeatures {
-    constructor(query, queryStr) {
-        this.query = query;
-        this.queryStr = queryStr
-    }
-    serach() {
-        const keyword = this.queryStr.keyword ? {
-            name: {
-                $regex: this.queryStr.keyword,
-                $options: "i",
-            }
-        } : {}
+  constructor(query, queryStr) {
+    this.query = query;
+    this.queryStr = queryStr;
+  }
+  serach() {
+    const keyword = this.queryStr.keyword
+      ? {
+          name: {
+            $regex: this.queryStr.keyword,
+            $options: "i",
+          },
+        }
+      : {};
 
-        this.query = this.query.find({ ...keyword });  //same as this.query.find({keyword:"iphone"})
-        
-        return this
-    }
-    filter() {
-        const queryCopy = { ...this.queryStr }
+    this.query = this.query.find({ ...keyword }); //same as this.query.find({keyword:"iphone"})
 
+    return this;
+  }
+  filter() {
+    const queryCopy = { ...this.queryStr };
 
-        //removing some fields for category
-        const removeFields = ["keyword", "page", "limit"]
-        removeFields.forEach((key) => delete queryCopy[key])
-        this.query = this.query.find(queryCopy)  //same as this.query.find({category:"mobile"})
-       
-        return this
+    //removing some fields for category
+    const removeFields = ["keyword", "page", "limit"];
+    removeFields.forEach((key) => delete queryCopy[key]);
 
-    }
+    //Filter for price and rating.
+
+    let queryStr = JSON.stringify(queryCopy);
+
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (key) => `$${key}`);
+    this.query = this.query.find(JSON.parse(queryStr)); //same as this.query.find({category:"mobile"})
+
+    return this;
+  }
+
+  pagination(resultPerPage) {
+    const currentPage = Number(this.queryStr.page) || 1;
+
+    const skip = resultPerPage * (currentPage - 1);
+
+    this.query = this.query.limit(resultPerPage).skip(skip);
+
+    return this;
+  }
 }
 
-module.exports = ApiFeatures
+module.exports = ApiFeatures;
