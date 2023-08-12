2f45b56927c04a5c2924c258719df99102d9365e
Vishal <vishaldas6566@gmail.com>
2023-08-12 12:15:01 +0530

    logout functionality added

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -5,7 +5,7 @@ const { isAuthenticatedUser } = require("../middlewares/auth")
 const router=express.Router()
 
 
-router.route("/products").get(isAuthenticatedUser, getAllProducts)
-router.route("/product/new").post(createProduct)
+router.route("/products").get(getAllProducts)
+router.route("/product/new").post(isAuthenticatedUser,createProduct)
 router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails)
 module.exports=router
