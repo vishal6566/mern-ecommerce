dc16c0b6f9e8fa2197f94fe0cd13a6466aba7caa
Vishal <vishaldas6566@gmail.com>
2023-08-13 20:34:33 +0530

    removed extra code

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -29,17 +29,3 @@ exports.authorizeRoles = (...roles) => {
   };
 };
 
-exports.authorizeRoles = (...roles) => {
-  return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      return next(
-        new ErrorHandler(
-          `Role: ${req.user.role} is not allowed to access this resource`,
-          403
-        )
-      );
-    }
-    next();
-  };
-};
-
