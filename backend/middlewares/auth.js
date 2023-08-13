ab53edf6d8399ec7febf83dda62cf0cf2360f33e
Vishal <vishaldas6566@gmail.com>
2023-08-13 20:33:48 +0530

    added

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -28,3 +28,18 @@ exports.authorizeRoles = (...roles) => {
     next();
   };
 };
+
+exports.authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorHandler(
+          `Role: ${req.user.role} is not allowed to access this resource`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
+
