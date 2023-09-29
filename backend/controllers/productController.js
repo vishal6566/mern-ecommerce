204ef85a10e4adc51923824359a8564b321c16ab
Vishal <vishaldas6566@gmail.com>
2023-09-29 19:41:32 +0530

    product review route and controller added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,6 +2,7 @@ const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncHandler = require("../middlewares/catchAsyncError");
 const ApiFeatures = require("../utils/apiFeatures");
+const catchAsyncError = require("../middlewares/catchAsyncError");
 //Create Product  --Admin
 exports.createProduct = catchAsyncHandler(async (req, res, next) => {
   req.body.user=req.user.id
@@ -73,3 +74,43 @@ exports.deleteProduct = catchAsyncHandler(async (req, res, next) => {
     message: "Product deleted successfully.",
   });
 });
+
+
+//Create new review or Update the review
+exports.createProductReview = catchAsyncError(async(req,res,next)=>{
+  const {rating,comment,productId}=req.body;
+  const review={
+    user:req.user._id,
+    name:req.user.name,
+    rating:Number(rating),
+    comment
+  }
+const product= await Product.findById(productId);
+
+ const isReviewed=product.reviews.find(rev=>rev.user.toString()===req.user._id.toString())
+
+if(isReviewed){
+product.reviews.forEach(rev=>{
+  if(rev=>rev.user.toString()===req.user._id.toString())
+    (rev.rating=rating),
+   ( rev.comment=comment);
+  
+ 
+})
+
+
+ }else{
+  product.reviews.push(review);
+  product.numOfReviews=product.reviews.length
+ }
+ let avg=0
+product.reviews.forEach(rev=>{
+  avg+=rev.rating
+ })
+ product.ratings=avg/product.reviews.length;
+await product.save({validateBeforeSave: false});
+res.status(200).json({
+  success:true
+})
+
+})
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 const express=require("express")
-const { getAllProducts,createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productController")
+const { getAllProducts,createProduct, updateProduct, deleteProduct, getProductDetails, createProductReview } = require("../controllers/productController")
 const { isAuthenticatedUser,authorizeRoles } = require("../middlewares/auth")
 
 const router=express.Router()
@@ -9,4 +9,5 @@ router.route("/products").get(getAllProducts)
 router.route("/admin/product/new").post(isAuthenticatedUser,authorizeRoles("admin"),createProduct)
 router.route("/admin/product/:id").put(isAuthenticatedUser,authorizeRoles("admin"),updateProduct).delete(isAuthenticatedUser,authorizeRoles("admin"),deleteProduct)
 router.route("/product/:id").get(getProductDetails)
+router.route("/review").put(isAuthenticatedUser,createProductReview )
 module.exports=router
